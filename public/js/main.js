4208ac80cf603b18de699fbb18d9e756c87061af
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-07-23 15:59:32 +0300

    change to project

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 function getLinkValue() {
-    var linkValue = document.getElementById("linkId").href;
+    var linkValue = document.getElementById("project").href;
     var linkValue = document.getElementById("skills").href;
     var linkValue = document.getElementById("about").href;
     var linkValue = document.getElementById("footer").href;
